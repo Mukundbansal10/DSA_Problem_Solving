de232ca6540fda9c9b3555e398357b05b6903c43
Mukund70 <mukundbansal10@gmail.com>
2024-06-08 23:19:15 +0530

    solve TwoSum problem using HashMap

--- src/Two_sum.java
+++ src/Two_sum.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Two_sum {
+import java.util.HashMap;
+
+public class Two_sum {
+    public static void main(String[] args) {
+        int[] arr = {11,3,7,9,14,2};
+        int target = 17;
+        int[] ans = new int[2];
+
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int secondNum = target - arr[i];
+            if(map.containsKey(secondNum)){
+                ans[0] =  map.get(secondNum);
+                ans[1] = i;
+                break;
+            }
+//            if the value is not in the map then collect it by below method
+            map.put(arr[i],i);
+        }
+        System.out.println(ans[0] + " "+ans[1]);
+
+    }
 }
