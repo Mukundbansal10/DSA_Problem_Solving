c1eab9adb92e54ac569132cca21de63697262a36
Mukund70 <mukundbansal10@gmail.com>
2024-06-10 17:38:30 +0530

    Smallest number problem using Factorial approach

---
+++ src/Arrays/Smallest_number.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.beans.Introspector;
+import java.util.Scanner;
+
+//Smallest number whose product of digits is N and N is given
+public class Smallest_number {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        System.out.println("solution(N) = " + solution(N));
+    }
+    public static int solution(int N){
+        String ans = "";
+        for (int i = 9; i >2; i--) {
+            while (N%i==0){
+                N = N/i;
+                ans= i+ans;
+            }
+        }
+        if(N!=1){
+            return -1;
+        }
+        else {
+            return Integer.parseInt(ans);
+        }
+    }
+}
